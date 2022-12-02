c6826c32379aa1b489328f7ea7ffb1cf854a0eda
Rian Lucas <rianluccas08@gmail.com>
2022-12-01 21:04:57 -0300

    Extraindo métodos para reutilização

--- recipes/tests/test_recipe_view.py
+++ recipes/tests/test_recipe_view.py
@@ -9,9 +9,6 @@ from .test_recipe_base import RecipeTestBase
 
 class RecipeViewsTest(RecipeTestBase):
 
-    def tearDown(self) -> None:
-        return super().tearDown()
-
     def test_recipe_home_view_function_is_correct(self):
         # It's taking the url name 'recipes:home' and reversing it to '/'.
         # Then it's resolving that url to the view function.
@@ -28,26 +25,23 @@ class RecipeViewsTest(RecipeTestBase):
         self.assertIs(view.func, views.recipe)
 
     def test_recipe_home_view_returns_status_code_200_OK(self):
+        self.make_recipe()
         response = self.client.get(reverse('recipes:home'))
         self.assertEqual(response.status_code, 200)
 
     def test_recipe_category_view_returns_status_code_200_OK(self):
+        self.make_recipe()
         response = self.client.get(
             reverse('recipes:category', kwargs={'category_id': 1}))
         self.assertEqual(response.status_code, 200)
 
     def test_recipe_home_view_loads_correct_templates(self):
+        self.make_recipe()
         response = self.client.get(reverse('recipes:home'))
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
 
-    def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):  # noqa
-        response = self.client.get(reverse('recipes:home'))
-        self.assertIn(
-            '<h1>No Recipes Yet :/</h1>',
-            response.content.decode('utf-8')
-        )
-
     def test_recipe_home_template_loads_recipes(self):
+        self.make_recipe()
         response = self.client.get(reverse('recipes:home'))
         content = response.content.decode('utf-8')
         response_context_recipes = response.context['recipes']
